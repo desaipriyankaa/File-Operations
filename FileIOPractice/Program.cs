d6f1a4d8084bdf6bd4b8016c8a3f5f65f556299b
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-05-18 10:36:58 +0530

    Add project files.

---
+++ FileIOPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FileIOPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"F:\PProject\Advanced\File IO\Start\Employee.txt";
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+
+            foreach(var line in lines)
+            {
+
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
